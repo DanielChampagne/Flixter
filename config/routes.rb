69e98eb2616070a3d8be90f1d30188949bafb018
Daniel Champagne <danchampagne@hotmail.com>
2019-10-29 01:30:20 +0000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
-    
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
